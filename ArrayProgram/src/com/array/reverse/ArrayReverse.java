bd2cd22cd391e9f9a6ceedafe8304ab1bcecbcec
AtharvaKapshikar <atharvakapshikar2002@gmail.com>
2024-11-23 23:39:46 +0530

    Initial commit

---
+++ ArrayProgram/src/com/array/reverse/ArrayReverse.java
@@ -0,0 +1,14 @@
+package com.array.reverse;
+
+public class ArrayReverse {
+
+	
+	public static void main(String[] args) {
+		
+		int[] arr= {1,2,3,4,5};
+		
+		for(int i = arr.length;i>0;i--) {
+			System.out.println("number is>>" + i);
+		}
+	}
+}
