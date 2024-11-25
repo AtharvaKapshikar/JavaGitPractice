1fa6d8c5caa3efe0b0c5f19e055f6883296747ed
AtharvaKapshikar <atharvakapshikar2002@gmail.com>
2024-11-25 16:23:16 +0530

    Program written for printing array

--- ArrayProgram/src/com/array/printing/ArrayPrinting.java
+++ ArrayProgram/src/com/array/printing/ArrayPrinting.java
@@ -2,4 +2,11 @@ package com.array.printing;
 
 public class ArrayPrinting {
 
+	public static void main(String[] args) {
+		int[] arr = {10,20,30,40,50};
+	
+		for(int i = 0;i<arr.length;i++) {
+			System.out.print(" " +  arr[i]);
+		}
+	}
 }
